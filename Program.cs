ccc9d2ec34e899e9828706707d957f529e490e65
yuliaokta <114564209+yuliaokta@users.noreply.github.com>
2022-09-27 19:40:09 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace stringconcatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+        }
+    }
+}
